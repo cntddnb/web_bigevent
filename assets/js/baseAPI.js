664a83f4ed159d21545c8991d827c6eb47216db1
tdd <cntddnb@gmail.com>
2022-10-22 17:45:01 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,7 +2,21 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中,可以闹到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
-//   console.log(options);
+  // console.log(options);
   options.url = "http://www.liulongbin.top:3007" + options.url;
-//   console.log(options.url);
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+
+  options.complete = function (res) {
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      localStorage.removeItem("token");
+      location.href = "/login.html";
+    }
+  };
 });
